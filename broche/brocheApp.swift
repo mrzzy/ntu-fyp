d955a0c9d51765f5ea3e23179cc68cfadaca37b3
Zhu Zhanyan <program.nom@gmail.com>
2026-06-04 17:35:18 +0800

    Initial Commit

---
+++ broche/brocheApp.swift
@@ -0,0 +1,17 @@
+//
+//  brocheApp.swift
+//  broche
+//
+//  Created by Zhu Zhanyan on 4/6/26.
+//
+
+import SwiftUI
+
+@main
+struct brocheApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ brocheTests/brocheTests.swift
@@ -0,0 +1,17 @@
+//
+//  brocheTests.swift
+//  brocheTests
+//
+//  Created by Zhu Zhanyan on 4/6/26.
+//
+
+import Testing
+@testable import broche
+
+struct brocheTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
