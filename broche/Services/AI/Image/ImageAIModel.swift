9ebf5fb4748e3d3d4be077640a36793717580ea9
Zhu Zhanyan <program.nom@gmail.com>
2026-07-24 11:55:04 +0800

    feat: add guidance & strength ImageAIOptions

---
+++ broche/Services/AI/Image/ImageAIModel.swift
@@ -0,0 +1,35 @@
+//
+//  ImageAIModel.swift
+//  broche
+//
+//  Created by Zhu Zhanyan on 2026-07-20.
+//
+
+import Foundation
+
+/// Image Editing AI Models
+struct ImageAIOptions {
+    /// The number of steps to perform during the editing process. More iterations may yield better results but take longer.
+    var steps: Int = 8
+    /// The scale factor for prompt guidance during the editing process. Higher values may produce more pronounced edits.
+    let guidance: Float
+    /// The strength of the editing effect. A value of 0.0 means no change from original image, while 1.0 means full application of the edits.
+    let strength: Float
+}
+
+/// Image Editing AI Model
+protocol ImageAIModel: AIModel {
+    /// Edits an image based on the provided prompt and options.
+    ///
+    /// - Parameters:
+    ///   - image: The input image to be edited.
+    ///   - prompt: The textual description guiding the edits.
+    ///   - options: Configuration controlling the editing process.
+    /// - Returns: The edited image.
+    /// - Throws: An error if editing fails or the model has not been loaded.
+    func edit(
+        image: Data,
+        prompt: String,
+        options: ImageAIOptions
+    ) async throws -> Data
+}
